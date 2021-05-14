c75b9d02f80a7bf5fa4f36168ebab62ab9ecaf98
janson_cheung <jansoncheung0004@gmail.com>
2021-05-15 01:49:45 +0800

    fix: add a widget for goal_today.dart

--- lib/views/today_view.dart
+++ lib/views/today_view.dart
@@ -1,3 +1,4 @@
+import 'package:aimimi/widgets/goal/goal_today.dart';
 import 'package:flutter/material.dart';
 import 'package:aimimi/styles/colors.dart';
 
@@ -22,68 +23,9 @@ class _TodayViewState extends State<TodayView> {
                     fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 16),
-              Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    color: Colors.white,
-                  ),
-                  width: MediaQuery.of(context).size.width,
-                  height: 76,
-                  padding: const EdgeInsets.only(left: 15),
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Drink Water",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: themeShadedColor,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          new Row(
-                            children: [
-                              Text(
-                                "Everyday",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: themeShadedColor,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              SizedBox(width: 8),
-                              Text(
-                                "30 days left",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: themeShadedColor,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                      SizedBox(width: 145),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "0/8",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: themeShadedColor,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ))
+              TodayGoal(),
+              TodayGoal(),
+              TodayGoal()
             ]));
   }
 }
