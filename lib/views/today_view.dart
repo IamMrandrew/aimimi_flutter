2672be1b41bf089eaca616a219af073612e80dae
janson_cheung <jansoncheung0004@gmail.com>
2021-05-15 01:40:26 +0800

    feat: add static ongoingGoals

--- lib/views/today_view.dart
+++ lib/views/today_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:aimimi/styles/colors.dart';
 
 class TodayView extends StatefulWidget {
   @override
@@ -8,6 +9,81 @@ class TodayView extends StatefulWidget {
 class _TodayViewState extends State<TodayView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        padding: EdgeInsets.only(top: 28, left: 25, right: 25),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "Two task left for today",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: themeShadedColor.withOpacity(0.6),
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.white,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: 76,
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Drink Water",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: themeShadedColor,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          new Row(
+                            children: [
+                              Text(
+                                "Everyday",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: themeShadedColor,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                "30 days left",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: themeShadedColor,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(width: 145),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "0/8",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: themeShadedColor,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ))
+            ]));
   }
 }
