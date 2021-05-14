fe62d2fb7213186dc42fe73cf1c531c0b83d5f6c
janson_cheung <jansoncheung0004@gmail.com>
2021-05-15 02:33:27 +0800

    style: add sizedbox

--- lib/views/today_view.dart
+++ lib/views/today_view.dart
@@ -24,7 +24,9 @@ class _TodayViewState extends State<TodayView> {
               ),
               SizedBox(height: 16),
               TodayGoal(),
+              SizedBox(height: 8),
               TodayGoal(),
+              SizedBox(height: 8),
               TodayGoal()
             ]));
   }
