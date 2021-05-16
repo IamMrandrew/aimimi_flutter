e8766779d1dba465338f99915b36a3a1f4cbcf1a
rpbanker <terrence521421@gmail.com>
2021-05-17 01:42:32 +0800

    feat: Add goal.dart and goal_view.dart.

--- lib/views/goal_view.dart
+++ lib/views/goal_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:aimimi/styles/colors.dart';
+import 'package:aimimi/widgets/goal/goal.dart';
 
 class GoalView extends StatefulWidget {
   @override
@@ -9,6 +10,17 @@ class GoalView extends StatefulWidget {
 class _GoalViewState extends State<GoalView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        padding: EdgeInsets.only(top: 28, left: 25, right: 25),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 16),
+              Goal(),
+              SizedBox(height: 8),
+              Goal(),
+              SizedBox(height: 8),
+              Goal()
+            ]));
   }
 }
---
+++ lib/widgets/goal/goal.dart
@@ -0,0 +1,78 @@
+import 'package:aimimi/styles/colors.dart';
+import 'package:flutter/material.dart';
+
+class Goal extends StatelessWidget {
+  const Goal({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          print("Clicked");
+        },
+        child: new Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.white,
+            ),
+            width: MediaQuery.of(context).size.width,
+            height: 76,
+            padding: const EdgeInsets.only(left: 15),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Drink Water",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: themeShadedColor,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    new Row(
+                      children: [
+                        Text(
+                          "Everyday",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: themeShadedColor,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          "30 days left",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: themeShadedColor,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(width: 145),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "›",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: themeShadedColor,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )));
+  }
+}
