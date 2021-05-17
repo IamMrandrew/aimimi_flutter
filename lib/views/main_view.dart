b19a9882593232a5080fb1796b852ca38e95e229
rpbanker <terrence521421@gmail.com>
2021-05-17 16:19:17 +0800

    feat: firebase connection

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -7,6 +7,9 @@ import 'package:aimimi/views/profile_view.dart';
 import 'package:aimimi/views/goal_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MainView extends StatefulWidget {
   @override
