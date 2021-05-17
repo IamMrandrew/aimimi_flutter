4d94999702fb957bd0a70387d4fc2e76763d932c
rpbanker <terrence521421@gmail.com>
2021-05-17 16:50:25 +0800

    feat: firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:aimimi/styles/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:aimimi/views/main_view.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
 
