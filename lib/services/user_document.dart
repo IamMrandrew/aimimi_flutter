1fab84022c8bb8d0d3011d1c4d677fb289354eee
rpbanker <terrence521421@gmail.com>
2021-05-22 16:04:23 +0800

    feat: users document

--- lib/services/user_document.dart
+++ lib/services/user_document.dart
@@ -2,20 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserDocument {
   final String uid;
-  bool newUser = false;
-
+  bool isNewUser = false;
   UserDocument({this.uid});
 
   final CollectionReference userDocument =
       FirebaseFirestore.instance.collection('users');
 
   Future createUserDocuments(createdAt, String username) async {
-    userDocument.doc(uid).get().then((value) {
-      if (value == null) {
-        newUser = true;
-      }
-    });
-    if (newUser) {
+    var a = await userDocument.doc(uid).get();
+    if (!a.exists) {
       return await userDocument.doc(uid).set({
         'createdAt': createdAt,
         'username': username,
